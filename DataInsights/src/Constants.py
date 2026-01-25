554642b4557fe6d2c0759060a75bca1557c99130
edebell67 <ed.ebell@gmail.com>
2026-01-25 18:45:38 +0000

    V20260125_1900: Infrastructure updates for Trade Drilldown Modal

--- DataInsights/src/Constants.py
+++ DataInsights/src/Constants.py
@@ -1,2 +1,2 @@
 # Application Constants
-VERSION = "V20260125_1650" # Updated after adding Rank By Trades mode - 2026-01-25 16:50
+VERSION = "V20260125_1900" # Updated after adding Trade Drilldown to Multi-Chart V3 - 2026-01-25 19:00
