3535ad11056112005d8825b8b867e32023df2e1f
test <test@test.com>
2026-05-07 02:35:16 +0100

    EP017: Move EP017 files to root

---
+++ ep_017_trader_pain_points/page_3_verifiable_data/verify_page.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Opening EP017 Landing Page 3...
+start "" "C:\Users\edebe\eds\epics\ep_017_trader_pain_points\page_3_verifiable_data\index.html"
+echo Ensure the API server is running on port 5017 for lead capture.
+pause
